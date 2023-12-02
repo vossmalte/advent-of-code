919f61944bec49fe7221c18807c7d6778997cf90
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-12-02 07:27:34 +0100

    ✨ (feat): solve first puzzle in haskell

---
+++ 1/first.hs
@@ -0,0 +1,33 @@
+-- import System.IO
+import Test.QuickCheck
+
+main :: IO ()
+
+main = do
+  contents <- readFile "input-first.txt"
+  -- putStrLn contents
+  let ls = filter (not . (==) []) (lines contents)
+  let onlyDigits = map (filter isDigit) ls
+  let onlyFirstAndLast = map firstAndLast onlyDigits
+  let asInt = map toInt onlyFirstAndLast
+  let result = sum asInt
+  print result
+
+  
+toInt :: String -> Int
+toInt x = read x :: Int
+
+prop_toInt :: Int -> Bool
+prop_toInt x = x ==  (toInt $ show x)
+
+digits = map (head . show) [1..9]
+
+isDigit = flip elem digits
+
+firstAndLast :: [a] -> [a]
+firstAndLast [] = []
+firstAndLast xs = (head xs):(last xs):[]
+
+prop_firstAndLast :: [a] -> Bool
+prop_firstAndLast [] = 0== length  (firstAndLast [])
+prop_firstAndLast xs = 2== (length  (firstAndLast xs))
