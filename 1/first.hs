ef894092173e41c4f640a46b97abb899eb013ee4
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-12-03 11:58:24 +0100

    ✨ (feat): solve day 1 part 2 in haskell

--- 1/first.hs
+++ 1/first.hs
@@ -1,8 +1,8 @@
 -- import System.IO
 import Test.QuickCheck
+import Data.List.Extra
 
 main :: IO ()
-
 main = do
   contents <- readFile "input-first.txt"
   -- putStrLn contents
@@ -13,6 +13,14 @@ main = do
   let result = sum asInt
   print result
 
+main2 :: IO ()
+main2 = do
+  contents <- readFile "input.txt"
+  let result = secondSolution $ lines contents
+  print result
+
+secondSolution :: [String] -> Int
+secondSolution lines = sum $ map ( toInt . firstAndLast . (filter isDigit) . replaceAllWords ) lines
   
 toInt :: String -> Int
 toInt x = read x :: Int
@@ -20,7 +28,19 @@ toInt x = read x :: Int
 prop_toInt :: Int -> Bool
 prop_toInt x = x ==  (toInt $ show x)
 
+numberWords = ["one","two","three","four","five","six","seven","eight","nine"]
+
 digits = map (head . show) [1..9]
+
+replaceTuple :: (String,Char) -> (String -> String)
+replaceTuple (word,digit) = replace word (word++[digit]++word)
+
+replacers :: [String -> String]
+replacers = map replaceTuple (zip numberWords digits)
+
+replaceAllWords :: String -> String
+-- replaceAllWords = sequence replacers -- does not work :(
+replaceAllWords = foldl (.) id replacers
 
 isDigit = flip elem digits
 
