55b5cfb57d5908da1a84fee1bc86c63a81f457cd
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2024-12-24 02:07:08 +0100

    ✨ (feat): solve 2024-23 part 2

--- 2024/23/solve.pl
+++ 2024/23/solve.pl
@@ -7,3 +7,19 @@ tripleWith1T(X,Y,Z) :- triple(X,Y,Z), startsWithT(X).
 triples(L) :- findall([X,Y,Z],tripleWith1T(X,Y,Z),L).
 triplesSet(S) :- triples(L), maplist(sort(),L,L1),sort(L1,L2),list_to_set(L2,S).
 solve1(X) :- triplesSet(S),length(S,X).
+
+connectsToAll(_, []).
+connectsToAll(Y,[X|R]) :- connected(X,Y), connectsToAll(Y,R).
+
+network([]).
+network([X|R]) :- connectsToAll(X,R), network(R).
+
+isSorted(L) :- sort(L,L).
+
+nw(1,[_]).
+nw(2,[X,Y]) :- connected(X,Y), isSorted([X,Y]).
+nw(S,[X|R]) :- SN is S - 1, nw(SN,R), isSorted(R), connectsToAll(X,R).
+
+degree(X,Degree) :- aggregate_all(count,connected(X,_),Degree).
+% ^ magic 13
+solve2(L) :- nw(13,L1), sort(L1,L).
